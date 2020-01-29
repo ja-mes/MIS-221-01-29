d69e6210995aa79c0f0761145ede6e184fe3b66a
unknown <jamezbrown100@gmail.com>
2020-01-29 17:25:52 -0600

    initial

---
+++ Class 01-29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Class_01_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            Console.WriteLine("Number before method: " + number);
+            UpdateNumber(number);
+            Console.WriteLine("Number after method: " + number);
+        }
+
+        static void UpdateNumber(int num)
+        {
+            num = num + 5;
+            Console.WriteLine("Number inside method: " + num);
+        }
+
+    }
+
+}
