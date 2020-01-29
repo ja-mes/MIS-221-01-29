425a5d3b8e677f9e24a0ce0abf0f319c34fb7ae3
unknown <jamezbrown100@gmail.com>
2020-01-29 17:44:41 -0600

    Update

--- Class 01-29/Program.cs
+++ Class 01-29/Program.cs
@@ -8,9 +8,22 @@ namespace Class_01_29
         {
             int number = 10;
 
+            // use ref to pass by reference
+
             Console.WriteLine("Number before method: " + number);
+            UpdateNumber(ref number); // number of parameters and arguments must be equal
+            Console.WriteLine("Number after ref method: " + number);
             UpdateNumber(number);
-            Console.WriteLine("Number after method: " + number);
+            Console.WriteLine("Number after val method: " + number);
+
+
+            Console.ReadKey();
+        }
+
+        static void UpdateNumber(ref int num)
+        {
+            num = num + 5;
+            Console.WriteLine("Number inside method: " + num);
         }
 
         static void UpdateNumber(int num)
